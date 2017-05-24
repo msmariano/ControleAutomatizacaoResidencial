ac9859ed48a1c888f322751b1e9ea0412f09c008
Marcelo dos Santos Mariano <marcelodossantosmariano@gmail.com>
2017-05-24 16:08:44 -0300

    Primeira versão

--- src/br/com/edu/pos/controle/automatizacao/residencial/view/ControleAutomatizacaoPrincipal.java
+++ src/br/com/edu/pos/controle/automatizacao/residencial/view/ControleAutomatizacaoPrincipal.java
@@ -1,11 +1,22 @@
 package br.com.edu.pos.controle.automatizacao.residencial.view;
 
+import java.util.List;
+
+import br.com.edu.pos.controle.automatizacao.residencial.sqlite.dao.Tabelas;
+
+
 public class ControleAutomatizacaoPrincipal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		//Teste
+		Tabelas Tabelas = new Tabelas("test.db");
+		List<String> l = Tabelas.getTabelas();
+		
+		for (String tabela : l) {
+			System.out.println(tabela);
+		}	
 
 	}
 
