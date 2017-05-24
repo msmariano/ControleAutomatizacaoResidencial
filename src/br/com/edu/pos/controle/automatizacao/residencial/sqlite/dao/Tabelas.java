b0872e19814917ed3df4179b1fc7476500b57644
Marcelo dos Santos Mariano <marcelodossantosmariano@gmail.com>
2017-05-24 16:08:23 -0300

    Primeira Versão

---
+++ src/br/com/edu/pos/controle/automatizacao/residencial/sqlite/dao/Tabelas.java
@@ -0,0 +1,60 @@
+package br.com.edu.pos.controle.automatizacao.residencial.sqlite.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tabelas extends JdbcDaoFactory{
+
+	private List<String> tabelas = new ArrayList<>();
+	String[] banco = {"","","",""};
+	
+	public Tabelas(String nomeDatabase){
+		setNomeDatabase(nomeDatabase);
+		listarTabelas();
+	}
+	
+	void insereTabela(String nomeTabela){
+		
+	}
+	void gerar(){
+		
+	}
+	
+	public Tabelas(){
+		listarTabelas();
+	}
+	public List<String> getTabelas() {
+		return tabelas;
+	}
+
+	public void setTabelas(List<String> tabelas) {
+		this.tabelas = tabelas;
+	}
+
+	public void listarTabelas(){
+		try {
+				
+				getConnection();
+				Statement stmt = connection.createStatement();
+				ResultSet rs = stmt.executeQuery( "SELECT name FROM sqlite_master WHERE type='table'" );
+				tabelas.clear();
+				while ( rs.next() ) {
+					tabelas.add(rs.getString("name"));
+				}
+				stmt.close();
+				finalizarConnecao();
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	boolean isTabelaExiste(String nomeTabela){
+		return tabelas.contains(nomeTabela);
+		 
+	}
+
+}
---
+++ src/br/com/edu/pos/controle/automatizacao/residencial/view/ControleAutomatizacaoPrincipal.java
@@ -0,0 +1,12 @@
+package br.com.edu.pos.controle.automatizacao.residencial.view;
+
+public class ControleAutomatizacaoPrincipal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Teste
+
+	}
+
+}
